835a0c60ff2ec5b5fafe0c80cd3bb78dc07f50a1
Nicholas Wallace <thewallacegames@gmail.com>
2021-04-14 13:42:36 -0500

    IEx

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public int Age { get; set; }
+
+        public int Legs { get; set; }
+
+        public bool HasBackbone { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -28,12 +28,45 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird moltres = new Bird()
+            {
+                Name = "Moltres",
+                Age = 1000,
+                Legs = 2,
+                HasBackbone = true,
+                IsWarmBlooded = true,
+                HasFeathers = false,
+                Wings = 2,
+                Type = "Fire",
+            };
+
+            Console.WriteLine($"{moltres.Name} is an {moltres.Type} pokemon that is {moltres.Age} years old and has " +
+                $"{moltres.Legs} legs and {moltres.Wings} wings.");
+            Console.WriteLine($"Is {moltres.Name} a vertebrate, warm blooded, and has feathers? " +
+                $"{moltres.HasBackbone}, {moltres.IsWarmBlooded}, and {moltres.HasFeathers}");
+            Console.WriteLine();
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            Reptile dragon = new Reptile()
+            {
+                Name = "Rayquaza",
+                Age = 2000,
+                Legs = 0,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                Environment = "Space",
+                Defense = "laser beam",
+            };
+
+            Console.WriteLine($"{dragon.Name} is a {dragon.Age} year old being that resides in {dragon.Environment}," +
+                $" {dragon.Defense}, and has {dragon.Legs} legs");
+            Console.WriteLine($"Is {dragon.Name} a vertebrate, cold blooded, and has scales?" +
+                $"{dragon.HasBackbone},{dragon.IsColdBlooded}, and {dragon.HasScales}");
+            Console.WriteLine();
         }
     }
 }
